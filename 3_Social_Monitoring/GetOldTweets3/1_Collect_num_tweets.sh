2f66438b13dd6e204955c01fbcaf3627b19f1a45
rian.hughes@physics.ox.ac.uk <rian.hughes@physics.ox.ac.uk>
2020-05-16 00:03:39 +0100

    Updated the script so that it sleeps for 3 mins regarless of the error (and sleeps for 3 mins and not 3*ii mins)

--- 3_Social_Monitoring/GetOldTweets3/1_Collect_num_tweets.sh
+++ 3_Social_Monitoring/GetOldTweets3/1_Collect_num_tweets.sh
@@ -1,7 +1,7 @@
 query_string="bitcoin"
-for day 	in 1 10 20;do
+for year        in 2017 2018;do     
+for day 	in 1;do
 for month 	in 1 2 3 4 5 6 7 8 9 10 11 12 ;do
-for year 	in 2017  ;do	
 	
 	time GetOldTweets3 --since $year-$month-$day --until $year-$month-$((day+1)) --querysearch $query_string --output A_Tweet_Data/A_GetOldTweet_$query_string-$year-$month-$day.csv 
 	echo $year-$month-$day
